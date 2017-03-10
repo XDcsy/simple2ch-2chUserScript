4114816ac79c6e27b900d4230312b134e279ddc3
neEverett <shengyang.cheng@outlook.com>
2017-03-10 22:20:56 +0800

    Implemented the 1st function.

--- line-feed.js
+++ line-feed.js
@@ -1,9 +1,12 @@
-function lineFeed
+var menuNamedTag = document.getElementsByName("menu");
+var tableTag = menuNamedTag[0].nextSibling;
+var aTags = tableTag.getElementsByTagName("a");
+var space = document.createElement("nobr"); 
+var parentNode = aTags[2].parentNode;
+space.innerHTML = "　";
+parentNode.insertBefore(space,aTags[2])
+for (var i=0; i < aTags.length; i++)
 {
-	var menuTag=document.getElementsByName("menu");
-	aTags = menuTag[0].nextSbiling.getElementsByTagName("a");
-	for (var i=0; i < aTags.length; i++)
-		{
-			aTags[i].innerHTML += "<br>";
-		}
+    if (aTags[i].innerHTML.length > 5)
+	    aTags[i].innerHTML += "<br>";
 }
